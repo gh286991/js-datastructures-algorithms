6c6e232b229b389a3e780b5b2dd476c2b24ff7ab
TomJhuang <gh286991@gmail.com>
2019-09-16 21:59:54 +0800

    hanoi tower

--- stack/hanoiTower.js
+++ stack/hanoiTower.js
@@ -1,32 +1,29 @@
 
-// import Stack from './stack'
 import Stack from './stackES6'
 
 
 function towerOfHanoi(n , start , target ,buffer){
-    // console.log('n' , n)
+
     if(n>0){
         towerOfHanoi(n-1, start, buffer, target); //把n-1個從start搬到buffer
-
+        console.log('n' , n)
         target.push(start.pop());   //把剩下的從start搬到target
-        console.log('=========')
-        // console.log('n = ' , n ,' ', +start.toString()+"->"+target.toString());
-        console.log("Move disc " + n + " from " + start + " to " + target );
-        // console.log('start' , start.toString())
-        // console.log('buffer' , buffer.toString())
-        // console.log('target' , target.toString())
-        console.log('==========')
-        
-        
+        console.log( `${start.name} = ${start.toString()}` )
+        console.log( `${buffer.name} = ${buffer.toString()}` )
+        console.log( `${target.name} = ${target.toString()}` )
+        console.log('------------')
         towerOfHanoi(n-1, buffer, target, start); //把n-1個從buffer搬到target
-        console.log('---------')
-        console.log("Move disc " + n + " from " + start + " to " + target );
-        console.log('---------')
-        //buffer >> to target
     }
 }
 
 const start = new Stack();
+const end = new Stack();
+const helper = new Stack();
+
+start['name'] = 'A'
+end['name'] = 'C'
+helper['name'] = 'B'
+
 // start.push(7);
 // start.push(6);
 // start.push(5);
@@ -35,14 +32,11 @@ start.push(3);
 start.push(2);
 start.push(1);
 
-const end = new Stack();
-const helper = new Stack();
+
 const N = start.size()
 
 start.print()
 
-// towerOfHanoi(N,start,end,helper)
-
-console.log('----end---')
 towerOfHanoi(N,start,end,helper)
+console.log('----end---')
 end.print()
